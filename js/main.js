356be2f9f417a1fd26224423d00d237fb4f076cd
nagah <nagah.shinawy@gmail.com>
2022-07-27 07:56:56 +0200

    where to put your code, diff between adding js code at the head tag vs the end of html page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 console.log("Hello World!");
 const DAYS = 365;
 console.log(DAYS);
+
+// when page load, run and execute code, but best practice to add js code at the end of html page
+window.onload = function () {
+    document.querySelector("h1").style.color = "red";
+
+};
+
