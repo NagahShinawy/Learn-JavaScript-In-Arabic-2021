e3b685c2e47eb8512105c7fa68309a32bf104410
nagah <nagah.shinawy@gmail.com>
2022-07-27 08:08:10 +0200

    comments

--- js/main.js
+++ js/main.js
@@ -2,11 +2,10 @@ console.log("Hello World!");
 const DAYS = 365;
 console.log(DAYS);
 
+// wait the window to load
 // when page load, run and execute code, but best practice to add js code at the end of html page
 // window.onload = function () {
 //     document.querySelector("h1").style.color = "red";
 //
 // };
 
-
-document.querySelector("h1").style.color = "red";
