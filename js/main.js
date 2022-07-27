99ee55e4b10d97a1100071919dd79bce2e3283fd
nagah <nagah.shinawy@gmail.com>
2022-07-27 07:58:34 +0200

    best parctice of place where to add your code

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ const DAYS = 365;
 console.log(DAYS);
 
 // when page load, run and execute code, but best practice to add js code at the end of html page
-window.onload = function () {
-    document.querySelector("h1").style.color = "red";
+// window.onload = function () {
+//     document.querySelector("h1").style.color = "red";
+//
+// };
 
-};
 
+document.querySelector("h1").style.color = "red";
