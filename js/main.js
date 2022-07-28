23094bd7f8bffed7797526eccf0896d5882458f1
nagah <nagah.shinawy@gmail.com>
2022-07-28 12:47:15 +0200

    console methods, directives, webapis

--- js/main.js
+++ js/main.js
@@ -12,3 +12,15 @@ console.log(DAYS);
 window.alert("Have a nice day!");
 document.write("<h1>JS is cool!</h1>", "<h1>Learn JS</h1>");
 console.log("Learn JS by example");
+console.error("Something happened!");
+console.table(['John', "Smith", "Leon", "Eric"]);
+console.table([
+    {"name": "John", "age": 20, "salary": 4000,},
+    {"name": "Smith", "age": 25, "salary": 2000,},
+    {"name": "Sara", "age": 21, "salary": 3000,},
+])
+
+// directives
+console.log("JS is %ccool", "color:red;");
+console.log("I %cLove %cJS", "color:red; font-size:30px;", "color:blue;font-size:40px;");
+
