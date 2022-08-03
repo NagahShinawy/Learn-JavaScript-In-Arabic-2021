6723f4936d2bc0bd9af15ad2325d6b30c6d3c514
nagah <nagah.shinawy@gmail.com>
2022-08-03 09:53:52 +0200

    Conditional (Ternary) Operator

--- js/main.js
+++ js/main.js
@@ -584,10 +584,45 @@ console.log(productPrice);
 
 
 
-// ============================== 31 - Comparison Operators    =======================================
+// ============================== 35 - Conditional Ternary Operator   =======================================
+
+let theName = "Mona";
+let theGender = "Female";
+let theAge = 30;
+
+if (theGender === "Male") {
+    console.log("Mr");
+} else {
+    console.log("Mrs");
+}
+
+// Condition ? If True : if False
+
+theGender === "Male" ? console.log("Mr") : console.log("Mrs");
+
+let result = theGender === "Male" ? "Mr" : "Mrs";
+
+document.write(result);
+
+console.log(theGender === "Male" ? "Mr" : "Mrs");
+
+console.log(`Hello ${theGender === "Male" ? "Mr" : "Mrs"} ${theName}`);
+
+theAge < 20
+    ? console.log(20)
+    : theAge > 20 && theAge < 60
+    ? console.log("20 To 60")
+    : theAge > 60
+    ? console.log("Larger Than 60")
+    : console.log("Unknown");
 
 
-
+let hasPermissions = true;
+hasPermissions === false ? console.log("Full Access"): console.log("Limited Access");
+let isMale = 1;
+let gender = isMale ? "Male": "Female";
+console.log(gender);
+console.log(`Hello ${theGender === 'Male'? "Mrs": "Ms"}, ${theName}`);
 // ============================== 31 - Comparison Operators    =======================================
 
 
