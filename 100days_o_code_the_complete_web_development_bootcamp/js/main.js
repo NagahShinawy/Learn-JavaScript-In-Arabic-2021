8c09846e214c883b2dce403835a9b102fdd224c5
nagah <nagah.shinawy@gmail.com>
2022-08-30 09:42:36 +0200

    loops

--- 100days_o_code_the_complete_web_development_bootcamp/js/main.js
+++ 100days_o_code_the_complete_web_development_bootcamp/js/main.js
@@ -6,7 +6,22 @@ let welcome = "Hello JS";
 // alert(welcome);
 
 let greetings = ["Welcome JS", "Hi JS Bootcamp", "Welcome Web Developer"]
+let names = ["Alex", "James", "John", "Adam"];
 // let greetings = []
 for (let i = 0; i < 10; i++) {
     console.log(new Random().choice(greetings));
+}
+console.log("*".repeat(30));
+// console.log(names.values());
+
+
+for (const name of names) {
+    console.log(`Hi ${name}`); // print name for each line
+}
+
+console.log("*".repeat(30));
+
+
+for (const name in names) {
+    console.log(`Hi ${name}`); // print index for each line
 }
